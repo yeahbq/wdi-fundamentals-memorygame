dc17863ed30732f4882d4c1e0fb040e928c11530
Barrett Quan <bquan@Barrett-Quans-MacBook-Pro.local>
2016-11-23 22:20:31 -0800

    created bool game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,5 +5,24 @@ var cardFour = "king";
 
 console.log("JS file is connected to HTML! Woo!")
 
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+}
+else
+alert ("Sorry, try again");
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+else
+alert ("Sorry, try again");
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+}
+else
+alert ("Sorry, try again");
+
+
 
 

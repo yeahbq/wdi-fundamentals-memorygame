8dced12573b645cb19f1ba2f993a544cd831213f
Barrett Quan <bquan@Barrett-Quans-MacBook-Pro.local>
2016-11-27 21:55:55 -0800

    Completed the memory game!

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,41 +1,79 @@
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
+// // var cardOne = "queen";
+// // var cardTwo = "queen";
+// // var cardThree = "king";
+// // var cardFour = "king";
 
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
 
-// if (cardOne === cardTwo) {
-// 	alert("You found a match!");
-// }
-// else
-// alert ("Sorry, try again");
+// // if (cardOne === cardTwo) {
+// // 	alert("You found a match!");
+// // }
+// // else
+// // alert ("Sorry, try again");
 
-// if (cardOne === cardThree) {
-// 	alert("You found a match!");
-// }
-// else
-// alert ("Sorry, try again");
+// // if (cardOne === cardThree) {
+// // 	alert("You found a match!");
+// // }
+// // else
+// // alert ("Sorry, try again");
 
-// if (cardOne === cardFour) {
-// 	alert("You found a match!");
-// }
-// else
-// alert ("Sorry, try again");
+// // if (cardOne === cardFour) {
+// // 	alert("You found a match!");
+// // }
+// // else
+// // alert ("Sorry, try again");
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
 
-var createBoard = function() {
-  for (var i=0; i<4; i++) {
+function createBoard () {
+  for (var i=0; cards.length; i++) {
 
     var cardElement = document.createElement('div');
 
     cardElement.className = 'card';
 
+    cardElement.setAttribute('data-card', cards[i]);
+
+    cardElement.addEventListener('click', isTwoCards);
+
     board.appendChild(cardElement);
+     board.appendChild(cardElement);
   }
 
 }
 
+function isTwoCards() {
+
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src= 'https://cdn.pixabay.com/photo/2015/08/11/11/57/diamonds-884195_960_720.png'>"; // king
+
+	} else {
+		this.innerHTML = "<img src = 'https://cdn.pixabay.com/photo/2015/08/11/11/57/clubs-884198_960_720.png'>"; //queen
+	}
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("WINNER! The two cards you chose matched");
+  } else {
+    alert("Sorry, that is not a match.");
+
+  }
+}
+
 createBoard();
+
+
 

1efc5baa8bf1a501f017d08ad7c3dcab18dd2831
Barrett Quan <bquan@Barrett-Quans-MacBook-Pro.local>
2016-11-27 19:26:44 -0800

    added js code for creating cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,28 +1,41 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
 console.log("JS file is connected to HTML! Woo!")
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
+// if (cardOne === cardTwo) {
+// 	alert("You found a match!");
+// }
+// else
+// alert ("Sorry, try again");
+
+// if (cardOne === cardThree) {
+// 	alert("You found a match!");
+// }
+// else
+// alert ("Sorry, try again");
+
+// if (cardOne === cardFour) {
+// 	alert("You found a match!");
+// }
+// else
+// alert ("Sorry, try again");
+
+var board = document.getElementById('game-board');
+
+var createBoard = function() {
+  for (var i=0; i<4; i++) {
+
+    var cardElement = document.createElement('div');
+
+    cardElement.className = 'card';
+
+    board.appendChild(cardElement);
+  }
+
 }
-else
-alert ("Sorry, try again");
 
-if (cardOne === cardThree) {
-	alert("You found a match!");
-}
-else
-alert ("Sorry, try again");
-
-if (cardOne === cardFour) {
-	alert("You found a match!");
-}
-else
-alert ("Sorry, try again");
-
-
-
+createBoard();
 
